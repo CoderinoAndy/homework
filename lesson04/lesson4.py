141db8a2495ce9396c511ac8bb4bfd20b5a4b4d4
CoderinoAndy <burritoman801@gmail.com>
2025-09-17 16:57:41 -0400

    lesson 4 complete

--- lesson04/lesson4.py
+++ lesson04/lesson4.py
@@ -1,1 +1,18 @@
 # Lesson 4
+def fenceplank():
+    fencePlank_number = 0
+    for _ in range(3):
+        fences_input = input()
+        fencePlank_number += len(fences_input)
+
+    paintLeftOver = 12 - fencePlank_number % 12
+
+    dozensrequired = round(fencePlank_number/12)
+    if dozensrequired == 0 and fencePlank_number != 0:
+        dozensrequired = 1
+
+
+    print(fencePlank_number)
+    print(paintLeftOver)
+    print(dozensrequired*14.95)
+fenceplank()
