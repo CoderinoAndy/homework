dde4f91bd52073f0d5f185aa9687957b29807386
CoderinoAndy <burritoman801@gmail.com>
2025-09-18 16:47:31 -0400

    created a program that calculates the # of cookies sold, the # of money profited, and the total money of the user

--- lesson05/lesson5.py
+++ lesson05/lesson5.py
@@ -1,1 +1,12 @@
 # Lesson 5
+initial_money = int(input())
+big_normal = input()
+cookies_sold = big_normal.len()
+big_sold = big_normal.count("b")
+normal_sold = big_normal.count("c")
+
+profit = 1.25*big_sold + 0.75*normal_sold
+
+print(f"You sold {big_sold + normal_sold} cookies today!")
+print(f"Today, you profited ${profit}")
+print(f"You got {profit + initial_money} after selling the cookies!")
