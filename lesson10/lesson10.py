a27910a21f40b709e040f16b2ea642977c461272
CoderinoAndy <burritoman801@gmail.com>
2025-09-19 09:56:44 -0400

    Very inefficient program but it functions fine

--- lesson10/lesson10.py
+++ lesson10/lesson10.py
@@ -1,1 +1,21 @@
 # Lesson 10
+switch = True
+number = input("Give me your number: ")
+while switch:
+    if number[0] == 8 or 9:
+        if number[1] == number[2]:
+            if number[3] == 8 or 9:
+                print("Call is from a telemarketer")
+                switch = False
+            else:
+                print("Call is fortunately not from a telemarketer")
+                break
+        else:
+            print("Call is fortunately not from a telemarketer")
+            break
+    else:
+        print("Call is fortunately not from a telemarketer")
+        break
+
+
+
