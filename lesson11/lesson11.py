354544e8ec23988e8a0edaa4cc2b904d80ca7850
CoderinoAndy <burritoman801@gmail.com>
2025-09-20 11:23:09 -0400

    created a fairly hard-coded program that determines where a user's coordinate is on the cartesian plane

--- lesson11/lesson11.py
+++ lesson11/lesson11.py
@@ -1,1 +1,21 @@
 # Lesson 11
+coordinate = input()
+coordinate_list = coordinate.split(",")
+listMapped = list(map(int, coordinate_list))
+x = listMapped[0]
+y = listMapped[1]
+
+if x > 0 and y > 0:
+    print(f"Quadrant 1: {x, y}")
+elif x > 0 and y < 0:
+    print(f"Quadrant 4: {x, y}")
+elif x < 0 and y > 0:
+    print(f"Quadrant 2: {x, y}")
+elif x < 0 and y < 0:
+    print(f"Quadrant 3: {x, y}")
+elif x == 0 and y == 0:
+    print(f"You are at the center: {x, y}")
+elif x == 0:
+    print(f"You are on the Y-axis: {x, y}")
+else:
+    print(f"You are on the X-axis: {x, y}")
