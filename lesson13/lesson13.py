1a1228a511eab70711aa661af33948da33c8c7e7
CoderinoAndy <burritoman801@gmail.com>
2025-09-20 16:34:41 -0400

    lesson13.py

--- lesson13/lesson13.py
+++ lesson13/lesson13.py
@@ -1,1 +1,9 @@
 # Lesson 13
+month = int(input())
+day = int(input())
+if (month, day) == (2, 18):
+    print("Special")
+elif (month, day) < (2, 18):
+    print("Before")
+else:
+    print("After")
