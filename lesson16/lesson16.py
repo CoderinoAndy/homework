2383d4059f32583dfa194016a1e695c2f82d7316
CoderinoAndy <burritoman801@gmail.com>
2025-10-01 10:55:20 -0400

    New file on fizzbuzz

--- lesson16/lesson16.py
+++ lesson16/lesson16.py
@@ -1,1 +1,10 @@
 # Lesson 16
+for number in range(1, 51):
+    if number%5 == 0:
+        print("Buzz")
+        if number%3 == 0:
+            print("FizzBuzz")
+    elif number%3 == 0:
+        print("Fizz")
+    else:
+        print(number)
