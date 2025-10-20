f50a6974b9a0258888b95ac47f3d59360d1ce299
CoderinoAndy <burritoman801@gmail.com>
2025-10-20 10:11:19 -0400

    yoyoyo fibonacci in the house

--- lesson22/lesson22.py
+++ lesson22/lesson22.py
@@ -1,1 +1,17 @@
 # Lesson 22
+def fibo(num):
+    # Location starts at 0
+    if num in [0, 1]:
+        return num
+    else:
+        first_back = 1
+        second_back = 0
+        location = 2
+        while location <= num:
+            current = first_back + second_back
+            second_back = first_back
+            first_back = current
+            location += 1
+        return current
+
+print(fibo(10))
