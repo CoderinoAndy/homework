336125d3bcd787eda822e9d78cfb6f816b54f3a4
CoderinoAndy <burritoman801@gmail.com>
2025-12-23 13:48:34 -0500

    yo

--- lesson32/lesson32.py
+++ lesson32/lesson32.py
@@ -1,1 +1,22 @@
 # Lesson 32
+def sharedchar(string1, string2):
+    final = []
+    combined = list(string1) + list(string2)
+    for x in combined:
+        if x not in final:
+            final.append(x)
+    swapped = True
+    while swapped:
+        swapped = False
+        for i in range(1, len(final) - 1):
+            if final[i - 1] > final[i]:
+                final[i - 1], final[i] = final[i], final[i - 1]
+                swapped = True
+    return final
+print(sharedchar("final", "jima"))        
+        
+
+    
+
+            
+
--- lesson33/lesson33.py
+++ lesson33/lesson33.py
@@ -1,1 +1,31 @@
 # Lesson 33
+mylist = []
+amountToAdd = int(input("amount to add: "))
+for x in range(amountToAdd):
+    toadd = int(input("What to add: "))
+    mylist.append(toadd)
+
+# finding the mean
+def mean(listy):
+    total = 0
+    count = 0
+    for x in mylist:
+        count += 1
+        total += x
+    return total/count
+
+def median(listy):
+    swapped = True
+    while swapped:
+        swapped = False
+        for x in range(1, len(listy)):
+            if listy[x - 1] > listy[x]:
+                listy[x - 1], listy[x] = listy[x], listy[x - 1]
+                swapped = True
+    if len(listy) % 2 == 0:
+        return (listy[len(listy)//2 - 1] + listy[len(listy)//2])/2
+    else:
+        return listy[len(listy)//2]
+
+print(mean(mylist))
+print(median(mylist))
